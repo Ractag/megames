40412bacde03d73780c080338bd32f2f64842a55
Victor G <victorgj95@outlook.com>
2024-06-17 10:31:35 +0200

    corrections about unnecessary imports un app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,10 @@ import { GameListComponent } from './Home/components/feature/game-list/game-list
 import { GameDetailComponent } from './Home/components/feature/game-detail/game-detail.component';
 import { GameListDumbComponent } from './Home/components/ui/game-list-dumb/game-list-dumb.component';
 import { GameDetailDumbComponent } from './Home/components/ui/game-detail-dumb/game-detail-dumb.component';
-import { LoginComponent } from './Login/components/login/login.component';
 import { RegisterComponent } from './Login/components/feature/register/register.component';
 import { LoginDumbComponent } from './Login/components/ui/login-dumb/login-dumb.component';
 import { RegisterDumbComponent } from './Login/components/ui/register-dumb/register-dumb.component';
-import { CartComponent } from './Payement/components/cart/cart.component';
-import { PaymentComponent } from './Payement/components/payment/payment.component';
 import { CartDumbComponent } from './Payement/components/ui/cart-dumb/cart-dumb.component';
-import { PaymentDimbComponent } from './Payement/components/ui/payment-dimb/payment-dimb.component';
 import { PaymentDumbComponent } from './Payement/components/ui/payment-dumb/payment-dumb.component';
 
 @NgModule({
@@ -24,21 +20,14 @@ import { PaymentDumbComponent } from './Payement/components/ui/payment-dumb/paym
     GameDetailComponent,
     GameListDumbComponent,
     GameDetailDumbComponent,
-    LoginComponent,
     RegisterComponent,
     LoginDumbComponent,
     RegisterDumbComponent,
-    CartComponent,
-    PaymentComponent,
     CartDumbComponent,
-    PaymentDimbComponent,
-    PaymentDumbComponent
+    PaymentDumbComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
